03b03e9d5fa58eea0ce1688fe181a28b0f4a9943
kil98q <killiandekok@gmail.com>
2017-01-23 13:25:57 +0100

    stuff

--- Crossy Road/Assets/JeroenMap/Scripts/Grid.cs
+++ Crossy Road/Assets/JeroenMap/Scripts/Grid.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour {
-	private Vector3 gridSize = new Vector3 (1, 1, 1);
-	private Vector3 movement = new Vector3 (0, 0, 1);
+	public Vector3 gridSize = new Vector3 (1, 1, 1);
+    // Scale is  1:10 1:100 1:100
+    public int Scale = 1;
+    private Node[,,] _grid;
 
+    Grid(Vector3 _gridSize)
+    {
+        gridSize = _gridSize;
+
+    }
 	// Use this for initialization
 	void Start () {
-		//InvokeRepeating ("UpdatePosition", 1.0,	1.0);
-		
 	}
-	
-	/*void UpdatePosition(){
+    public Vector3 ConvertCordinates(Vector3 cords)
+    {
+        return new Vector3(Mathf.Abs(cords.x), Mathf.Abs(cords.y), Mathf.Abs(cords.z));
+    }
+    public Node GetNode(Vector3 cords)
+    {
+        return _grid[(int)cords.x, (int)cords.y, (int)cords.z];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //InvokeRepeating ("UpdatePosition", 1.0,	1.0);
+    /*void UpdatePosition(){
 		Vector3 newPos = transform.position + movement;
 		newPos = Vector3(Mathf.Round(newPos.x/gridSize.x)*gridSize.x,
 			Mathf.Round(newPos.y/gridSize.y)*gridSize.y,
---
+++ Crossy Road/Assets/JeroenMap/Scripts/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public GameObject Object;
+    public bool IsWalkable;
+    public void Reset()
+    {
+        IsWalkable = true;
+        Object = null;
+    }
+}
