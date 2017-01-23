565a2c3db72dbdeda7e9da292918b3b42dc95ee2
kil98q <killiandekok@gmail.com>
2017-01-23 14:22:27 +0100

    Clean slate

---
+++ Crossy Road/Assets/Scripts/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour {
+    public float duration = 0;
+    public float time = 0;
+    public bool Enabled = false;
+    public bool Repeat = false;
+    public UnityEvent timer_Trigger;
+
+    public Timer(float _duration)
+    {
+        duration = _duration;
+    }
+	// initialization
+	void Start () {
+        if (timer_Trigger == null)
+        {
+            timer_Trigger = new UnityEvent();
+        }
+        time = duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enabled)
+        {
+            if(time == 0)
+            {
+                Enabled = false;
+                timer_Trigger.Invoke();
+                if (Repeat)
+                {
+                    Reset();
+                }
+            }
+            else
+            {
+                if (time < 0)
+                {
+                    time = 0;
+                }
+                else
+                {
+                    time -= Time.deltaTime;
+                }
+            }
+        }
+	}
+    // resets the timer time to duration
+    public void Reset()
+    {
+        Enabled = true;
+
+        time = duration;
+    }
+}
---
+++ Crossy Road/Assets/SoufyanMap/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void sceneLoad()
+    {
+        StartCoroutine("WaitTime");
+        StartCoroutine("ChangeScene");
+    }
+
+    IEnumerator WaitTime()
+    {
+        SceneManager.LoadScene("testscene");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("SoufyanScene");
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(1);
+    }
+}
